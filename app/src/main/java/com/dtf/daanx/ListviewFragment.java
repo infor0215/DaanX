bade6f8db5d748a56de347f3f262c630260bf1dd
yoyo930021 <yoyo930021@gmail.com>
2015-12-18 12:54:29 +0800

    優化抽屜效能與最新公告製作

--- app/src/main/java/com/dtf/daanx/ListviewFragment.java
+++ app/src/main/java/com/dtf/daanx/ListviewFragment.java
@@ -3,6 +3,8 @@ package com.dtf.daanx;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +33,7 @@ import java.util.HashMap;
 public class ListviewFragment extends Fragment {
 
     SharedPreferences preference;
+    ArrayList<PostList> jsonArr;
     int page=1;
 
     @Override
@@ -44,6 +47,7 @@ public class ListviewFragment extends Fragment {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    //region forum
                     try {
                         Document doc = Jsoup.connect("https://api.dacsc.club/daanx/forum/main/"+page)
                                 .timeout(5000)
@@ -53,11 +57,11 @@ public class ListviewFragment extends Fragment {
                         Log.i("json",data);
 
                         Gson gson = new Gson();
-                        Type listType = new TypeToken<ArrayList<Post>>() {
+                        Type listType = new TypeToken<ArrayList<ForumList>>() {
                         }.getType();
-                        ArrayList<Post> jsonArr = gson.fromJson(data, listType);
+                        ArrayList<ForumList> jsonArr = gson.fromJson(data, listType);
                         ArrayList<HashMap<String, Object>> itemList = new ArrayList<>();
-                        for (Post obj:jsonArr) {
+                        for (ForumList obj:jsonArr) {
                             HashMap<String, Object> temp = new HashMap<>();
                             temp.put("title", obj.getTitle());
                             temp.put("writer", obj.getWriter().charAt(0));
@@ -95,13 +99,82 @@ public class ListviewFragment extends Fragment {
                     }catch (Exception e){
                         Log.e("TAG", e.getMessage(), e);
                     }
+                    //endregion
+                }
+            }).start();
+        }else if(str.equals("week")){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    //region week
+                    try {
+                        Document doc = Jsoup.connect("https://api.dacsc.club/daanx/post/week/"+page)
+                                .timeout(5000)
+                                .data("auth",preference.getString("auth",""))
+                                .get();
+                        String data=doc.select("body").text();
+                        Log.i("json",data);
+
+                        Gson gson = new Gson();
+                        Type listType = new TypeToken<ArrayList<PostList>>() {
+                        }.getType();
+                        jsonArr = gson.fromJson(data, listType);
+                        ArrayList<HashMap<String, Object>> itemList = new ArrayList<>();
+                        for (PostList obj:jsonArr) {
+                            HashMap<String, Object> temp = new HashMap<>();
+                            temp.put("title", obj.getTitle());
+                            temp.put("writer", obj.getWriter().charAt(0));
+
+//                            String image[]=obj.image.split("|||");
+//                            String images="<br>";
+//                            for(int i=0;i<image.length;i++){
+//                                images+="<img src="+image[i]+" /><br>";
+//                            }
+                            String base64=new String(Base64.decode(obj.getBody().getBytes("UTF-8"),Base64.DEFAULT),"UTF-8");
+                            doc = Jsoup.parse(base64);
+                            String tempBosy=doc.text();
+                            temp.put("body",tempBosy);
+                            itemList.add(temp);
+                        }
+                        final SimpleAdapter adapter = new SimpleAdapter(
+                                getActivity(),
+                                itemList,
+                                R.layout.post_row,
+                                new String[]{"title", "writer", "body"},
+                                new int[]{R.id.title, R.id.image, R.id.content}
+                        );
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                SuperListview listView = (SuperListview) view.findViewById(R.id.list);
+                                listView.setAdapter(adapter);
+                                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView arg0, View arg1, int arg2,
+                                                            long arg3) {
+                                        // TODO Auto-generated method stub
+                                        ListView listView = (ListView) arg0;
+                                        Toast.makeText(
+                                                getActivity(),
+                                                "ID：" + arg3 +
+                                                        "   選單文字："+ listView.getItemAtPosition(arg2).toString(),
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+                            }
+                        });
+                    }catch (Exception e){
+                        Log.e("TAG", e.getMessage(), e);
+                    }
+                    //endregion
                 }
             }).start();
         }
         return view;
     }
 
-    public class Post {
+    public class ForumList {
 
         @SerializedName("title")
         private String title;
@@ -111,9 +184,6 @@ public class ListviewFragment extends Fragment {
 
         @SerializedName("writer")
         private String writer;
-
-        @SerializedName("file")
-        private String file;
 
         public String getTitle() {
             return title;
@@ -139,6 +209,28 @@ public class ListviewFragment extends Fragment {
             this.writer = writer;
         }
 
+    }
+
+    public class PostList {
+
+        @SerializedName("title")
+        private String title;
+
+        @SerializedName("body")
+        private String content;
+
+        @SerializedName("writer")
+        private String writer;
+
+        @SerializedName("file")
+        private String file;
+
+        @SerializedName("image")
+        private String image;
+
+        @SerializedName("link")
+        private String link;
+
         public String getFile() {
             return file;
         }
@@ -147,9 +239,47 @@ public class ListviewFragment extends Fragment {
             this.file = file;
         }
 
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getBody() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getWriter() {
+            return writer;
+        }
+
+        public void setWriter(String writer) {
+            this.writer = writer;
+        }
 
     }
-
     //                RequestQueue mQueue = Volley.newRequestQueue(getActivity());
 //                StringRequest stringRequest = new StringRequest("",
 //                        new Response.Listener<String>() {
